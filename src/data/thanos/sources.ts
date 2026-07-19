18dc71d20e4a51c3ce696afae7aa66bc1b7db077
kiraplane <zhh0504@gmail.com>
2026-07-19 10:22:02 +0800

    chore: apply July 17 site updates

--- src/data/thanos/sources.ts
+++ src/data/thanos/sources.ts
@@ -1,7 +1,7 @@
 import type { DataSource } from './types';
 
-export const CHECKED_AT = '2026-07-14';
-export const OFFICIAL_CHECKED_AT = '2026-07-14';
+export const CHECKED_AT = '2026-07-17';
+export const OFFICIAL_CHECKED_AT = '2026-07-17';
 
 export const officialGameFacts = {
   name: '[ Update ] Infinity Gauntlet | Thanos Simulator',
