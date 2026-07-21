84307a2a1847d18600c76ef9821a6c69cb476fb8
kiraplane <zhh0504@gmail.com>
2026-07-21 13:16:04 +0800

    chore: apply July 21 growth updates

--- src/data/thanos/codes.ts
+++ src/data/thanos/codes.ts
@@ -1,7 +1,7 @@
 import { officialGameFacts } from './sources';
 import type { GameCode } from './types';
 
-export const CODE_CHECKED_AT = '2026-07-20';
+export const CODE_CHECKED_AT = '2026-07-21';
 
 export const activeCodes: GameCode[] = [];
 
