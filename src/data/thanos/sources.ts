2319a88d0812b6584320c39d8d0b583896da37c4
kiraplane <zhh0504@gmail.com>
2026-08-03 12:38:45 +0800

    chore: refresh site growth data for 2026-08-03

--- src/data/thanos/sources.ts
+++ src/data/thanos/sources.ts
@@ -1,7 +1,7 @@
 import type { DataSource } from './types';
 
-export const CHECKED_AT = '2026-08-01';
-export const OFFICIAL_CHECKED_AT = '2026-08-01';
+export const CHECKED_AT = '2026-08-03';
+export const OFFICIAL_CHECKED_AT = '2026-08-03';
 
 export const officialGameFacts = {
   name: '[ Update ] Infinity Gauntlet | Thanos Simulator',
